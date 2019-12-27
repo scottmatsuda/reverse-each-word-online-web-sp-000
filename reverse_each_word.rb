790edb21b8206e95b8ef19b1d703d64aefefdb58
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-27 00:58:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,14 @@
-def reverse_each_word(string)
-  reverse = string.split(" ").collect do |word|
+def reverse_each_word
+  reverse = string.split(" ").each do |word|
     word.reverse
   end
   return reverse.join(" ")
 end
+
+
+# def reverse_each_word(string)
+#   reverse = string.split(" ").collect do |word|
+#     word.reverse
+#   end
+#   return reverse.join(" ")
+# end
